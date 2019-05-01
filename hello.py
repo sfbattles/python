a45c6ead2a72ee2b6a26e1b656d584c133f467f5
Richard Long <richard.long.e@gmail.com>
2019-05-01 11:35:17 -0400

    sample python code

---
+++ hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.7
+
+# my_name = "POTTER=DAVID=LOUIS"
+# my_list = my_name.split("=")
+# lastname_list = [my_list[0]]
+# firstname_list = my_list[1:] 
+# display_list = firstname_list + lastname_list
+
+# print(*display_list, sep = " ")
+
+s ='hello'
+print(s[::-1])
+
+
